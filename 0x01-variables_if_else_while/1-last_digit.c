7bdf304354e5ee0485639715e9e07885dd9f0432
olufunkee <volufunke610@gmail.com>
2022-03-14 12:53:11 -0700

    Assign a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,53 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-#include <time.h>
-
-#include <stdlib.h>
-
 /**
+ * main - Determines either greater than 5, is less than 6, or is 0
  *
- *  * main - main block
- *
- *   * Description: Get a random number and check its last digit, compare it with 5
- *
- *    * Return: 0
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int n, 1;
 
-		int n;
-
-			int last;
-
-
-
-				srand(time(0));
-
-					n = rand() - RAND_MAX / 2;
-
-						last = n % 10;
-
-
-
-							if (last > 5)
-
-										printf("Last digit of %i is %i and is greater than 5\n", n, last);
-
-								else if (last == 0)
-
-											printf("Last digit of %i is %i and is 0\n", n, last);
-
-									else if (last < 6)
-
-												printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
-
-
-
-										return (0);
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
 }
-
-
+	
