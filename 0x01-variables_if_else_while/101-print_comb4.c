9518167879ce8c00c6a934aa621cb0ad2e37661b
olufunkee <volufunke610@gmail.com>
2022-03-14 20:27:49 -0700

    Print comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,12 +24,12 @@ int main(void)
 					putchar('0' + e);
 					putchar('0' + d);
 					putchar('0' + c);
+					
 					if (c + d + e != 9 + 8 + 7)
 					{
-						break;
-					}
 					putchar(',');
 					putchar(' ');
+					}
 				}
 				c++;
 			}
