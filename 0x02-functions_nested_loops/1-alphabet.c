37ee2ef1455ae00ff00d85af018364458b7950ab
olufunkee <volufunke610@gmail.com>
2022-03-16 14:23:50 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char f = 'a';
+
+	while (f <= 'z')
+	{
+		putchar(f);
+		f++;
+	}
+	return (0);
+}
