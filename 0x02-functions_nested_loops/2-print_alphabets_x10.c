e2c84bd4c860f109e74994bd8f136890bd799e26
olufunkee <volufunke610@gmail.com>
2022-03-17 09:31:15 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabets_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- this function is to print
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+
+	for (n = 0; n <=10; n++)
+	{
+		for (f = a; f <= z; f++)
+		{
+		_putchar(f);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
