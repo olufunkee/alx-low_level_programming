f05272d7692835382a3035fbe62bd35d81ee34e2
olufunkee <volufunke610@gmail.com>
2022-03-18 04:00:52 -0700

    Print 1 if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - to print 1 if lowercase
+ *
+ * @c - the parameter used
+ *
+ * Return: value int
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+return (0);
+}
