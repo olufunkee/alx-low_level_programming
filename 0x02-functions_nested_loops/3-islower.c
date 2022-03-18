802fc66dfbe63c8ef4b1d379306f0e06a6e5dfd3
olufunkee <volufunke610@gmail.com>
2022-03-18 04:22:07 -0700

    Pritn 1 if lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,15 +7,14 @@
  */
 int _islower(int c)
 {
-char c = 'a'
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
-_putchar('\n');
-return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+	return (0);
 }
