358423fcd78b797e22ec8580daad1fef72fb9831
olufunkee <volufunke610@gmail.com>
2022-03-19 10:20:49 -0700

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,10 @@ void print_most_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		for (n == 2 && n ==4)
+		if (n != 2 && n != 4)
 		{
-			continue
-		}
 		_putchar(n + '0');
+		}
 	}
 	_putchar('\n');
 }
