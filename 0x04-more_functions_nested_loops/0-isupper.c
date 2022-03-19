a4980a0cbf4c4a6734d78b881349d6d99b1d3dd9
olufunkee <volufunke610@gmail.com>
2022-03-19 08:54:40 -0700

    Print 1 if uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	char c = 'A';
+
+	if (c = 'A')
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	_putchar('\n');
+	return (0);
+}
