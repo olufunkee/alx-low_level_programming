967bc7599b4cbb622725227e346d44eb982c7fd0
olufunkee <volufunke610@gmail.com>
2022-03-20 02:45:35 -0700

    Print Fizzbuzz

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,7 +3,7 @@
 
 /**
  * print_triangle - Print triangle dependent on the parameter size.
- * @size : The height of the triangle
+ * @size: The height of the triangle
  * Return: Void.
  */
 void print_triangle(int size)
@@ -12,15 +12,15 @@ void print_triangle(int size)
 	int x;
 	int spaces;
 
-	for (i =0; i < size, i++)
+	for (i = 0; i < size, i++)
 	{
 		for (spaces = size - 1 - i; spaces > 0; spaces--)
 		{
-			_putchar(' ')
+			_putchar(' ');
 		}
 		for (x = 0; x <= i; x++)
 		{
-			_putchar('#')
+			_putchar('#');
 		}
 		_putchar('\n');
 	}
