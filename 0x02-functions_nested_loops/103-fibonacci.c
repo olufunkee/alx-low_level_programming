03b9919eab6f968d4943822403e67ba35ad7398c
olufunkee <volufunke610@gmail.com>
2022-03-22 06:59:54 -0700

    Find and print the even-valued terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - fibonacci 50
+ * main - Entry point
  *
- * Return:  (Success)
+ * purpose - fibonacci
+ *
+ * Return:  (0) always
  */
 
 int main(void)
-
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
+	unsigned long int lim = 4000000;
+	unsigned long int bef = 0;
+	unsigned long int aft = 1;
+	unsigned long int R = 0;
+	unsigned long int sum = 0;
 
-	printf("%lu", bef);
-
-	for (i = 1; i < 50; i++)
+	while (lim > R)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;								}
-	printf("\n");
+		R = bef + aft;
+
+		if ((R % 2) == 0)
+		{
+			sum += R;
+		}
+
+		bef = aft;
+		aft = R;
+	}
+	printf("%li\n", sum);
 	return (0);
 }
