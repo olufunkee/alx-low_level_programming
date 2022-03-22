a5a3c0477e81928de0f5c9ee3b199cb3435ae275
olufunkee <volufunke610@gmail.com>
2022-03-22 04:35:40 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,8 +14,9 @@ int _putchar(char c);
 
 void print_number(int n)
 {
+
 	unsigned int n1 = 0;
-	
+
 	if  (n < 0)
 	{
 		n1 = -n;
@@ -31,5 +32,6 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
+	
 	_putchar((n1 % 10) + '0');
 }
