2ac634a554206165a5ab34c3cdaab5392bb2816c
olufunkee <volufunke610@gmail.com>
2022-03-22 04:07:14 -0700

    Print number

--- 0x04-more_functions_nested_loops/101-print_numbers.c
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -6,32 +6,28 @@ int _putchar(char c);
 
 /**
  * print_number - like a hello world
- *
  * @n: params an integer
- *
  * No return
  */
 
 void print_number(int n)
 {
 	unsigned int n1 = 0;
-
-	if (n < 0)
+	
+	if  (n < 0)
 	{
 		n1 = -n;
-		_putchar('_');
+		_putchar('-');
 	}
-
+	
 	else
 	{
 		n1 = n;
 	}
-
+	
 	if (n1 / 10)
 	{
 		print_number(n1 / 10);
 	}
-
 	_putchar((n1 % 10) + '0');
-	_putchar('\n');
 }
