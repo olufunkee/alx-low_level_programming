844d91773887a17607b717e1ae7de7f280573c55
olufunkee <volufunke610@gmail.com>
2022-03-22 04:38:52 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -22,16 +22,16 @@ void print_number(int n)
 		n1 = -n;
 		_putchar('-');
 	}
-	
+
 	else
 	{
 		n1 = n;
 	}
-	
+
 	if (n1 / 10)
 	{
 		print_number(n1 / 10);
 	}
-	
+
 	_putchar((n1 % 10) + '0');
 }
