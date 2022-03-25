c6ed7e54c99de728411ae61bb34bb2cf07b523c1
olufunkee <volufunke610@gmail.com>
2022-03-25 03:00:20 -0700

    Cat two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,6 +6,7 @@
  * @src: param pointer to a char
  * Return: return value of dest
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i;
@@ -13,7 +14,7 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	j = 0;
-	
+
 	while (dest[i] != '\0')
 	{
 		i++;
