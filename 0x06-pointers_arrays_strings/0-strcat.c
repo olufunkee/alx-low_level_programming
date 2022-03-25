045059d0c0b4efb09a69e9acd6758fa32cf1c999
olufunkee <volufunke610@gmail.com>
2022-03-25 02:58:01 -0700

    cat two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * _strcat - function commute strings
+ * *_strcat - function commute srtings
  * @dest: param pointer to a char
  * @src: param pointer to a char
  * Return: return value of dest
  */
-
-char *-strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
-
+	
 	while (dest[i] != '\0')
 	{
 		i++;
