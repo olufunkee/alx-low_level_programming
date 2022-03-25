64739dd1dc669d2bb3a19cc35bf5e2b98dc06655
olufunkee <volufunke610@gmail.com>
2022-03-25 03:05:37 -0700

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncpy - two words
+ * *_strncpy - two words
  * @dest : pointer to char params
  * @src : pointer to char params
  * @n : int params
@@ -11,12 +11,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	
+
 	for ( ; i < n; i++)
 	{
 		dest[i] = '\0';
