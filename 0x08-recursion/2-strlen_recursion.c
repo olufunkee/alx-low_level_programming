c7149189ad82282b4fcc4aafb32c034506a6deba
olufunkee <volufunke610@gmail.com>
2022-03-30 18:10:46 -0700

    Return the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _strlen_recursion(char *s)- size
+ * @s: pointer to string params
+ * Return: recursion
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return(0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
