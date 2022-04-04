62101e7ccc21ff0d18bedce2402c55c9a128f8aa
olufunkee <volufunke610@gmail.com>
2022-04-04 04:58:08 -0700

    Print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,15 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-/
+int main(int argc, char* argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < 1; i++)
+	{
+		_putchar(argv[i]);
+	}
+	_putchar('\n')
+	return 0;
+}
