9500170cb38d0649cc5fd4527d84d2ddc4085f54
olufunkee <volufunke610@gmail.com>
2022-04-04 06:52:33 -0700

    Print number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - function
- * @argc: offset count
- * @argv: offset value
+ * main - prints its name, followed by a new line
+ * @argc: n args
+ * @argv: arr args
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%s\n", argc);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 	return (0);
 }
