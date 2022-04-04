8a9976d45c842b03df4dfeb7aa05db5171932545
olufunkee <volufunke610@gmail.com>
2022-04-04 06:34:39 -0700

    Print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+
 int main(int argc, char* argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++);
-	{
-		print("argv[%d] = %s\n", i, argv[]);
-	}
+	printf("%s\n", argv[argc - 1]);
 	return 0;
 }
