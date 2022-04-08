40cb7fa0883a76db3f93cd211b9bede4f9414111
olufunkee <volufunke610@gmail.com>
2022-04-07 18:11:03 -0700

    Allocate memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,12 +6,11 @@
  * @b: An unsigned input integer
  * Return: Apointer to allocated memory or NULL if it fails
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *s;
 
-	s= malloc(b);
+	s = malloc(b);
 
 	if (s == NULL)
 		exit(98);
